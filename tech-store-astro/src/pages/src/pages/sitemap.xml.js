fc75d5c6134d18c35ab81ec0e724910bb8ca699a
moptee <sho.sakamoto17219@gmail.com>
2026-06-08 06:14:51 +0900

    sitemap手動追加

---
+++ tech-store-astro/src/pages/src/pages/sitemap.xml.js
@@ -0,0 +1,20 @@
+import { getAllProducts } from '../lib/microcms.js';
+
+export async function GET() {
+  const products = await getAllProducts();
+  const siteUrl = 'https://mopgadge1.com';
+
+  const urls = [
+    `<url><loc>${siteUrl}/</loc></url>`,
+    ...products.map(p => `<url><loc>${siteUrl}/products/${p.id}/</loc></url>`),
+  ].join('\n');
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+}
